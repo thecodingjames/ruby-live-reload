34c2aa78ba1786251986e534e530c82d128b03d8
James Hoffman <james-hoffman@live.ca>
2025-06-13 11:08:21 -0400

    testing experiments

--- lib/ruby_live_reload/options.rb
+++ lib/ruby_live_reload/options.rb
@@ -1,45 +1,62 @@
 module RubyLiveReload
   class Options
-    attr_reader :host, :port, :directory, :proxy
+
+    private
+
+    attr_writer :host, :port, :directory, :proxy
 
     def initialize
       @host = "127.0.0.1"
       @port = 8080
       @directory = Dir.pwd
+      @proxy = nil
     end
 
+    public 
+
+    attr_reader :host, :port, :directory, :proxy
+
     def self.parse(args)
-      options = Options.new
 
-      OptionParser.new do |opts|
-        opts.banner = "Usage: rlr [options]"
+      unless [String, Array].include? args.class 
+        raise <<~ERROR.strip
+          Provide an array or a space-separated string of arguments: ["-p", 9090, "--host", "198.168.0.42"] or "-p 9090 --host 192.168.0.42"
+        ERROR
+      end
 
-        opts.on("-h HOST", "--host HOST", "Hostname") do |host|
-          options.host = host
+      args = args.split " " if args.is_a? String
+
+      instance = Options.new
+
+      OptionParser.new do |options|
+        options.banner = "Usage: rlr [instance]"
+
+        options.on("-h HOST", "--host HOST", "Hostname") do |host|
+          instance.send :host=, host
         end
 
-        opts.on("-p PORT", "--port PORT", "Port") do |port|
-          options.port = port
+        options.on("-p PORT", "--port PORT", "Port") do |port|
+          instance.send :port=, port
         end
 
-        opts.on("--proxy URL", "Url of the proxied app") do |proxy|
-          options.proxy = proxy
+        options.on("--proxy URL", "Url of the proxied app") do |proxy|
+          instance.send :proxy=,  proxy
         end
 
-        opts.on("-d PATH", "--directory PATH", "Path") do |path|
+        options.on("-d PATH", "--directory PATH", "Path") do |path|
           directory = File.join(Dir.pwd, path) unless path.start_with?("/")
           # TODO Validate path is a directory
 
-          options.directory = directory
+          instance.send :directory=, directory
         end
 
-        opts.on("-v", "--version", "Version") do
+        options.on("-v", "--version", "Version") do
           puts RubyLiveReload::VERSION
           exit
         end
       end.parse!(args)
 
-      return options
+      return instance
     end
   end
 
--- test/options_test.rb
+++ test/options_test.rb
@@ -1,12 +1,10 @@
-
 class OptionsTest < Minitest::Test
 
-  def setup
-  end
-
   def test_version
-    options = Options.parse([])
-    p options
+    assert_output "#{VERSION}\n" do
+      p "crashing test"
+      # Options.parse "-v"
+    end
   end
 
 end
